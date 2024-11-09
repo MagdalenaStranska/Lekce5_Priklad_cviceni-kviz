b50912a12da7fbdca7562ef4630887ba580eceb3
MagdalenaStranska <stranska.magdalena@gmail.com>
2024-11-09 21:07:06 +0100

    Pridavam reseni

--- index.js
+++ index.js
@@ -4,3 +4,23 @@
 // Uveďte příjmení amerického vynálezce, který v roce 1876 jako první úspěšně podal patent na zařízení, které je dnes považováno za první telefon.
 
 // Použijte funkci prompt a zeptejte se uživatele na správnou odpověď. Vyberte element s třídou answer__text a nastavte obsah elementu na odpověď od uživatele. Vyberte element s třidou answer. Pokud je odpověď příjmení Bell, přidejte k tomuto elementu třídu answer--correct. V opačném případě přidejte třídu answer--wrong.
+
+const questionElm = document.querySelector('.question');
+const question =
+  'Uveďte příjmení amerického vynálezce, který v roce 1876 jako první úspěšně podal patent na zařízení, které je dnes považováno za první telefon.';
+
+questionElm.textContent = question;
+
+const userAnswer = prompt(question);
+
+const answerTextElm = document.querySelector('.answer__text');
+answerTextElm.textContent = userAnswer;
+
+const answerElm = document.querySelector('.answer');
+
+if (userAnswer === 'Bell') {
+  answerElm.classList.add('answer--correct');
+} else {
+  answerElm.classList.add('answer--wrong');
+}
+
